fcb77f6ac497e7b5dac9fdc29bb93577dbd519a7
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-10-29 20:58:00 +0530

    On Item Add update Shop

--- api/controllers/InvoiceExpenditureController.js
+++ api/controllers/InvoiceExpenditureController.js
@@ -12,6 +12,69 @@ var controller = {
     },
     getAllItemsName: function (req, res) {
         InvoiceExpenditure.getAllItemsName(req.body, res.callback);
-    }
+    },
+    save: function (req, res) {
+        console.log(req.body)
+        req.model.saveData(req.body, function (err, data) {
+            console.log(err)
+            if (err) {
+                console.log("Here In Error")
+            } else {
+                Shop.find({}, {
+                    name: 1
+                }).lean().exec(function (err, shopsArr) {
+                    console.log("data._id", data._id)
+                    var invoiceObject = {
+                        item: data._id,
+                        quantity: 0
+                    };
+                    console.log("InvoiceObj", invoiceObject);
+                    async.eachSeries(shopsArr, function (n, callback) {
+                        Shop.update({
+                            "_id": n._id
+                        }, {
+                            $addToSet: {
+                                items: invoiceObject
+                            }
+                        }).lean().exec(function (err, Shop) {
+                            if (err) {
+                                callback();
+                            } else {
+                                callback();
+                            }
+                        })
+                    }, function (err) {
+                        if (err) {
+                            res.callback(err, data);
+                        } else {
+                            res.callback(null, data);
+                        }
+                    });
+                })
+            }
+        });
+    },
+    // async.eachSeries(newInsurer, function (n, callback) {
+    //                 Customer.getChildCustomer({
+    //                     "_id": n
+    //                 }, function (err, data2) {
+    //                     if (err) {
+    //                         callback();
+    //                     } else {
+    //                         arr = _.concat(arr, data2);
+    //                         callback();
+    //                     }
+    //                 })
+    //             }, function (err) {
+    //                 if (err) {
+
+    //                 } else {
+    //                     _.each(arr, function (n) {
+    //                         req.body.insurer.push(n);
+    //                     });
+    //                     req.body.insurer = _.uniq(req.body.insurer)
+    //                     req.model.getAll(req.body, res.callback, req.user);
+    //                 }
+    //             });
 };
 module.exports = _.assign(module.exports, controller);
